0c5e545c86c9566a409b2ed209ede9e7093423bc
karaulovskin <karaulovskin@yandex.ru>
2021-04-03 20:42:11 +0300

    add config, Authentication failed.

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('App!');
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        app.listen(PORT, () => console.log(`App has been started on ${PORT}...`))
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+start()
