ed98fa6e453ee5b34e3df2f688425309b04288fd
karaulovskin <karaulovskin@yandex.ru>
2021-04-24 15:43:52 +0300

    edit User.js

--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ const {Schema, model, Types} = require('mongoose')
 
 // базовая схема для пользователя
 const schema = new Schema({
-    email: {type: String, require: true, unique: true},
-    password: {type: String, require: true},
-    links: [{type: Types.Object, ref: 'Link'}]
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    links: [{type: Types.ObjectId, ref: 'Link'}]
 })
 
-module.export = model('User', schema)
+module.exports = model('User', schema)
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,10 @@
 const {Router} = require('express')
+const User = require('../models/User')
+const router = Router()
 const bcrypt = require('bcryptjs')
 const config = require('config')
 const jwt = require('jsonwebtoken')
 const {check, validationResult} = require('express-validator')
-const User = require('../models/User')
-const router = Router()
 
 // /api/auth/register
 router.post(
